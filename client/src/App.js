a6eddfdb40a888c1ed4b463a19b5f5e777e0bc8c
sofialevin <slevin@M-4876.local>
2019-09-14 09:26:02 -0700

    Fetched data from server using a class component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      players: []
+    }
+  }
 
+  componentDidMount() {
+    fetch("http://localhost:5000/api/players")
+      .then(res => res.json())
+      .then(players => this.setState({ players: players }))
+      .catch(err => console.log("noooo: ", err));
+  }
+
+  render() {
+    console.log(this.state.players) 
+    return (
+    <div>
+
+    </div>);
+  }
+}
+ 
 export default App;
