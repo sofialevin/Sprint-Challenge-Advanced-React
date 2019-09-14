4e6938f622e062b3cdb6ca554dc274dc1db17127
sofialevin <slevin@M-4876.local>
2019-09-14 12:01:52 -0700

    Loaded favorites from localStorage

--- client/src/components/PlayerCards.js
+++ client/src/components/PlayerCards.js
@@ -14,25 +14,20 @@ const PlayerCards = (props) => {
       : setFavorites([...favorites, id])
     }
   
+console.log(favorites)
+
     return (
       
         <Card.Group>
         {
           props.players.map((player) => 
-          favorites.includes(player.id) ? <Card key={player.id} id={player.id}>
+          <Card key={player.id} id={player.id}>
           <Card.Content>
-            <Card.Header>{player.name} <Rating icon='heart' onClick={handleClick} defaultRating={1} maxRating={1} /></Card.Header>
-            <Card.Meta>{player.country}</Card.Meta>
-          </Card.Content>
-          <Card.Content extra>
-          <a>
-            <Icon name='search' />
-            {player.searches}
-          </a>
-        </Card.Content>       
-          </Card> : <Card key={player.id} id={player.id}>
-          <Card.Content>
-            <Card.Header>{player.name} <Rating icon='heart' onClick={handleClick} defaultRating={0} maxRating={1} /></Card.Header>
+            <Card.Header>{player.name} 
+            {
+              favorites.includes(player.id.toString()) ? <Rating icon='heart' onClick={handleClick} defaultRating={1} maxRating={1} /> : <Rating icon='heart' onClick={handleClick} defaultRating={0} maxRating={1} />
+            }
+            </Card.Header>
             <Card.Meta>{player.country}</Card.Meta>
           </Card.Content>
           <Card.Content extra>
